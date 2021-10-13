b70b9de43c68698eef26ba5988f3e94763dc9d50
Kristian Luukkonen <kluukkon@edu.lapinamk.fi>
2021-10-13 16:00:37 +0300

    Update PlayerController.cs
    Updated movement system to use free movement instead of tile based.
    
    Also allowed use of keyboard for quick testing and also in preparations to switch using joysticks on controller.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,32 +3,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
-    Pelaajan liikkumisen ideana on että kenttä on jaettu ruutuihin joiden välillä
-    pelaaja liikkuu yhden ruudun kerrallaan.
-
-    Jos ohjaimeen vaihdetaan joystick liikkumista varten tällöin voidaan unohtaa tämä
-    systeemi liikkumiseen ja käyttää vapaata liikkumista.
-*/
-
 public class PlayerController : SerialController
 {
+    private CharacterController controller;
+
+    [Header("PlayerController specific variables")]
+    [Space(15)]
     [SerializeField]
     private bool isMoving;
-
     public bool IsMoving
     {
         get { return isMoving; }
     }
 
+    [SerializeField]
+    private bool keyboardMovement; // Allows keyboard to be used for movement
+
+    [Space(10)]
+    [SerializeField]
+    [Range(1, 15)]
+    [Tooltip("Speed that character rotates to face the point that it is moving towards")]
+    private float rotationSpeed;
+    [SerializeField]
+    [Range(1, 10)]
+    private float movementSpeed;
+
+    private void Awake()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
     void Update()
     {
+        // Used for quickly testing out features on keyboard instead of using controller
+        // Probably also could be used for joystick movement with slight modifications
+        if (keyboardMovement)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            Vector3 newPos = new Vector3(transform.position.x + horizontal, transform.position.y, transform.position.z + vertical);
+            Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+            Vector3 dir = newPos - transform.position;
+
+            if (direction.magnitude >= 0.1f)
+            {
+                Quaternion rotation = Quaternion.LookRotation(dir);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+                controller.Move(direction * movementSpeed * Time.deltaTime);
+            }
+        }
+
         string message = (string)serialThread.ReadMessage();
         if (message == null)
             return;
 
-        // Lähettää ohjaimelta tulevat viestit SampleMessageListenerille, joka sitten
-        // Debug.Log:aa vastaanotetun viestin
+        // Passes incoming messages from controller to SampleMessageListener, which then logs the message
         if (ReferenceEquals(message, SERIAL_DEVICE_CONNECTED))
             messageListener.SendMessage("OnConnectionEvent", true);
         else if (ReferenceEquals(message, SERIAL_DEVICE_DISCONNECTED))
@@ -36,10 +66,7 @@ public class PlayerController : SerialController
         else
             messageListener.SendMessage("OnMessageArrived", message);
 
-
-        // Tarkistaa ettei pelaaja liiku ennenkuin tarkistaa
-        // ohjaimelta saadun viestin mihin suuntaan pelaaja
-        // haluaa liikkua
+        // Checks that player is not moving before checking received message
         if (!isMoving)
             if (message == "UP" || message == "DOWN" || message == "RIGHT" || message == "LEFT")
                 StartCoroutine(Move(message));
@@ -59,11 +86,29 @@ public class PlayerController : SerialController
 
     private IEnumerator Move(string direction)
     {
+        isMoving = true;
+
         Vector3 targetPos = transform.position;
 
-        // Laskee uuden positionin pelaajan inputin mukaan
+        // Calculates new position based on input
         switch (direction)
         {
+            case "UPLEFT":
+                targetPos.z += 1;
+                targetPos.x -= 1;
+                break;
+            case "UPRIGHT":
+                targetPos.z += 1;
+                targetPos.x += 1;
+                break;
+            case "DOWNLEFT":
+                targetPos.z -= 1;
+                targetPos.x -= 1;
+                break;
+            case "DOWNRIGHT":
+                targetPos.z -= 1;
+                targetPos.x += 1;
+                break;
             case "UP":
                 targetPos.z += 1;
                 break;
@@ -81,26 +126,13 @@ public class PlayerController : SerialController
                 break;
         }
 
-        isMoving = true;
-
+        // Calculates direction and rotation that player is going to face when they start moving
         Vector3 dir = targetPos - transform.position;
-
         Quaternion rotation = Quaternion.LookRotation(dir);
-
-        /* Jotain bugaa välillä että jää jumiin tuohon while loopiin.
-           Ei ole tärkeä ominaisuus vielä joten korjaamisen voi jättää myöhemmälle
-
-        while (transform.rotation != rotation)
-        {
-            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, 4 * Time.deltaTime);
-            yield return new WaitForEndOfFrame();
-        }
-        */
-
-        transform.LookAt(targetPos);
 
         while (transform.position != targetPos)
         {
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, 10 * Time.deltaTime);
             float step = 4 * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, targetPos, step);
             yield return new WaitForEndOfFrame();
@@ -119,9 +151,7 @@ public class PlayerController : SerialController
         throw new NotImplementedException();
     }
 
-
-    // Ei enää käytössä oleva metodi.
-    // Voi poistaa jos ei uutta käyttötarkoitusta löydy
+    // Method not in use. Can be deleted if no other uses for it
     private Vector3 GetPosition(Transform transform)
     {
         return transform.position;
