4f8fbf6107815c0038d130d3fba2558188318358
rase <=>
2021-11-30 12:10:27 +0200

    Uusi arduino koodi

--- Assets/Arduino/ohjain/ohjain.ino
+++ Assets/Arduino/ohjain/ohjain.ino
@@ -1,7 +1,6 @@
 #include <OneButton.h>
 #include "BluetoothSerial.h"
 
-
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
@@ -15,6 +14,7 @@ BluetoothSerial SerialBT;
 #define BUTTON1 27
 #define BUTTON2 14
 #define BUTTON3 12
+#define LED 5
 
 OneButton upButton = OneButton(
   UP_BUTTON,
@@ -55,8 +55,8 @@ OneButton button3 = OneButton(
 void setup() {
   // put your setup code here, to run once:
   //Serial.begin(9600);
-  SerialBT.begin("ESP32");
-
+  SerialBT.begin("OHJAIN");
+  
   upButton.attachClick(UpButtonClick);
   upButton.attachLongPressStop(UpButtonClick);
   downButton.attachClick(DownButtonClick);
@@ -87,65 +87,85 @@ void setup() {
   pinMode(BUTTON1, INPUT_PULLUP);
   pinMode(BUTTON2, INPUT_PULLUP);
   pinMode(BUTTON3, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
 }
 
 int leftButtonStatus = 0;
 int rightButtonStatus = 0;
 int upButtonStatus = 0;
 int downButtonStatus = 0;
+bool connected = false;
 
+
+// put your main code here, to run repeatedly:
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
+
+  // If serial monitor receives a message, check if its a connect or disconnect message.
+  if (Serial.available())
+  {
+      String incomingMessage = Serial.readString();
+      if (incomingMessage == "Disconnected")
+      {
+        connected = false;
+      }
+      else if (incomingMessage == "Connected")
+      {
+        connected = true;
+      }
   }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
-  }
-  
-  int leftPinValue = digitalRead(LEFT_BUTTON);
-  int rightPinValue = digitalRead(RIGHT_BUTTON);
-  int upPinValue = digitalRead(UP_BUTTON);
-  int downPinValue = digitalRead(DOWN_BUTTON);
+      
+    ConnectionStatus(connected);
 
-  upButton.tick();
-  downButton.tick();
-  leftButton.tick();
-  rightButton.tick();
-  button1.tick();
-  button2.tick();
-  button3.tick();
-
-  // Jos aiheutuu lagia serialin lukemisessa niin delaytä voi nostaa suuremmaksi.
-  delay(40);
-
-  if(digitalRead(LEFT_BUTTON) == LOW){
-    // Lähetettävän arvon tulisi olla Horizontal -1
-    //Serial.println("1");
-    SerialBT.println("1");
-  }
-
-
-  if (digitalRead(RIGHT_BUTTON) == LOW){
-    // Lähetettävän arvon tulisi olla Horizontal 1
-    //Serial.println("3");
-    SerialBT.println("3");
-  }
-
-
-  if (digitalRead(UP_BUTTON) == LOW){
-    // Lähetettävän arvon tulisi olla Vertical 1
-    //Serial.println("-3");
-    SerialBT.println("-3");
-  }
-
-
-  if (digitalRead(DOWN_BUTTON) == LOW){
-    // Lähetettävän arvon tulisi olla Vertical -1
-    //Serial.println("-1");
-    SerialBT.println("-1");
-  }
-
+    // If bluetooth connection has been established.
+    if (connected)
+    {
+      int leftPinValue = digitalRead(LEFT_BUTTON);
+      int rightPinValue = digitalRead(RIGHT_BUTTON);
+      int upPinValue = digitalRead(UP_BUTTON);
+      int downPinValue = digitalRead(DOWN_BUTTON);
+    
+      upButton.tick();
+      downButton.tick();
+      leftButton.tick();
+      rightButton.tick();
+      button1.tick();
+      button2.tick();
+      button3.tick();
+    
+      // Jos aiheutuu lagia serialin lukemisessa niin delaytä voi nostaa suuremmaksi.
+      delay(40);
+    
+      if(digitalRead(LEFT_BUTTON) == LOW)
+      {
+        // Lähetettävän arvon tulisi olla Horizontal -1
+        //Serial.println("1");
+        SerialBT.println("1");
+      }
+    
+    
+      if (digitalRead(RIGHT_BUTTON) == LOW)
+      {
+        // Lähetettävän arvon tulisi olla Horizontal 1
+        //Serial.println("3");
+      }
+    
+    
+      if (digitalRead(UP_BUTTON) == LOW)
+      {
+        // Lähetettävän arvon tulisi olla Vertical 1
+        //Serial.println("-3");
+        SerialBT.println("-3");
+      }
+    
+    
+      if (digitalRead(DOWN_BUTTON) == LOW)
+      {
+        // Lähetettävän arvon tulisi olla Vertical -1
+        //Serial.println("-1");
+        SerialBT.println("-1");
+      }
+      
+    }
 }
 
 // Lopettaa pelaajan liikkumisen siihen suuntaan mitä nappia ei enää pidetä pohjassa.
@@ -186,4 +206,20 @@ static void ButtonTwoClick(){
 static void ButtonThreeClick(){
   //Serial.println("7");
   SerialBT.println("7");
+}
+
+
+static void ConnectionStatus(bool status)
+{
+  if (status)
+  {
+    digitalWrite(LED, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED, LOW);
+    delay(500);
+    digitalWrite(LED, HIGH); 
+    delay(500);
+  }
 }
